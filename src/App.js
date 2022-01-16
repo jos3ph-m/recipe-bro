4d3c8fd2e443d6e036a2e58d2e5e3a610ae8979e
Joseph M <jmedina1985@hotmail.com>
2022-01-15 16:22:02 -0800

    Router & Pages setup

--- src/App.js
+++ src/App.js
@@ -13,7 +13,9 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Switch></Switch>
+        <Switch>
+          <Home />
+        </Switch>
       </BrowserRouter>
     </div>
   );
