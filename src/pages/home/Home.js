00dd6d1044a6006f06e3bcfb6208c7d721426ece
Joseph M <jmedina1985@hotmail.com>
2022-01-19 13:18:52 -0800

    fetching data

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -5,5 +5,5 @@ import './Home.css';
 
 export default function Home() {
   const { data, isPending, error } = useFetch('http://localhost:3000/recipes');
-  return <div className="home"></div>;
+  return <div className="home">{error && <p>{error}</p>}</div>;
 }
