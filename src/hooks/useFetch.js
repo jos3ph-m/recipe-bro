c7426c6ce3a61ef5ac4cce62ddf0da52db225daa
Joseph M <jmedina1985@hotmail.com>
2022-01-18 19:31:41 -0800

    fetching data

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,4 +2,5 @@ import { useState, useEffect } from 'react';
 
 export const useFetch = (url) => {
   const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(false);
 };
