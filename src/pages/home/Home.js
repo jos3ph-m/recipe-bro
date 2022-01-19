3644cadd43277dd891da2d2f704f8d7c75e92baf
Joseph M <jmedina1985@hotmail.com>
2022-01-19 13:21:26 -0800

    fetching data

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className="home">
       {error && <p className="error">{error}</p>}
-      {isPending}
+      {isPending && <p></p>}
     </div>
   );
 }
