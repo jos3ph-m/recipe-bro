cce9419a9d0c97870c46b17b5abce10382045dac
Joseph M <jmedina1985@hotmail.com>
2022-01-18 19:32:46 -0800

    fetching data

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -4,4 +4,10 @@ export const useFetch = (url) => {
   const [data, setData] = useState(null);
   const [isPending, setIsPending] = useState(false);
   const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    const fetchData = async () => {};
+  });
 };
