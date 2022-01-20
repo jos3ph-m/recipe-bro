46ca569bb7720d6e2f055f55d3b11f0954c0b332
Joseph M <jmedina1985@hotmail.com>
2022-01-20 15:16:05 -0800

    Recipe list component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,10 @@
-import RecipeList from '../../components/RecipeList';
 import { useFetch } from '../../hooks/useFetch';
 
 //styles
 import './Home.css';
+
+//components
+import RecipeList from '../../components/RecipeList';
 
 export default function Home() {
   const { data, isPending, error } = useFetch('http://localhost:3000/recipes');
