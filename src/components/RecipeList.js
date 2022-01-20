1e5ba9e4404b68a38be4e16b33b5e956d31c6867
Joseph M <jmedina1985@hotmail.com>
2022-01-20 15:18:12 -0800

    Recipe list component

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -2,5 +2,11 @@
 import './RecipeList.css';
 
 export default function RecipeList({ recipes }) {
-  return <div className="recipe-list"></div>;
+  return (
+    <div className="recipe-list">
+      {recipes.map((recipe) => (
+        <div key={recipe.id}></div>
+      ))}
+    </div>
+  );
 }
