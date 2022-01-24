78755a7161f3921e6b6e05f3dd4df3f185a0df87
Joseph M <jmedina1985@hotmail.com>
2022-01-24 14:19:43 -0800

    Recipe list component

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -8,5 +8,7 @@ export default function Recipe() {
   const { id } = useParams();
   const url = `http://localhost:3000/recipes/${id}`;
 
+  const { error, isPending, data: recipe } = useFetch(url);
+
   return <div>Recipe</div>;
 }
