5a33e32891c3af1009581304099a616b11fb2183
Joseph M <jmedina1985@hotmail.com>
2022-01-24 14:17:45 -0800

    Recipe list component

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,6 +1,10 @@
+import { useParams } from 'react-router-dom';
+
 //styles
 import './Recipe.css';
 
 export default function Recipe() {
+  const { id } = useParams();
+
   return <div>Recipe</div>;
 }
