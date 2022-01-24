1bc7047af91518e607fa07d9d1d0c4ca4affbc76
Joseph M <jmedina1985@hotmail.com>
2022-01-24 14:18:54 -0800

    Recipe list component

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,10 +1,12 @@
 import { useParams } from 'react-router-dom';
+import { useFetch } from '../../hooks/useFetch';
 
 //styles
 import './Recipe.css';
 
 export default function Recipe() {
   const { id } = useParams();
+  const url = `http://localhost:3000/recipes/${id}`;
 
   return <div>Recipe</div>;
 }
