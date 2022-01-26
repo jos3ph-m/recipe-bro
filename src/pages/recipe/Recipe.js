c282a0c20a452491a12d70912fdc0f81ba7c011f
Joseph M <jmedina1985@hotmail.com>
2022-01-25 18:04:55 -0800

    Recipe details template

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -14,7 +14,7 @@ export default function Recipe() {
     <div className="recipe">
       {error && <p className="error">{error}</p>}
       {isPending && <p className="loading">Loading...</p>}
-      {recipe && ()}
+      {recipe && <></>}
     </div>
   );
 }
