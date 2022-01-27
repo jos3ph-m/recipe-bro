1b96c50415d34e24f957456161f3314de92b8ded
Joseph M <jmedina1985@hotmail.com>
2022-01-27 11:55:09 -0800

    Making Create Recipe

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -14,6 +14,7 @@ export default function Create() {
       <form>
         <label>
           <span>Recipe title:</span>
+          <input type="text" />
         </label>
       </form>
     </div>
