c6865fe5cb0386f597e38defe6e741be32e46f27
Joseph M <jmedina1985@hotmail.com>
2022-01-27 11:52:41 -0800

    Making Create Recipe

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -6,6 +6,7 @@ import './Create.css';
 export default function Create() {
   const [title, setTitle] = useState('');
   const [method, setMethod] = useState('');
+  const [cookingTime, setCookingTime] = useState('');
 
   return <div>Create</div>;
 }
