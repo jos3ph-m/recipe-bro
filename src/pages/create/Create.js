aa2de3306cc98a18fb3edb24ebc6edc6053d337e
Joseph M <jmedina1985@hotmail.com>
2022-01-29 12:53:27 -0800

    Making Create Recipe

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -23,7 +23,7 @@ export default function Create() {
         </label>
         <label>
           <span>Recipe method:</span>
-          <textarea onChange={(e) => setTitle()} />
+          <textarea onChange={(e) => setTitle(e)} />
         </label>
       </form>
     </div>
