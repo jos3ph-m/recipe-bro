b724ff1a60268843acaafac2e84a81696b4f6dc7
Joseph M <jmedina1985@hotmail.com>
2022-01-30 13:07:20 -0800

    Making Create Recipe

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -26,6 +26,7 @@ export default function Create() {
             required
           />
         </label>
+        {/* ingredients go here */}
         <label>
           <span>Recipe method:</span>
           <textarea
