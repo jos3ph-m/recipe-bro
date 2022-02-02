29f6ed65908a3d1b676147a0bd54a6240a356db5
Joseph M <jmedina1985@hotmail.com>
2022-02-02 14:44:25 -0800

    Making a POST request

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-export const useFetch = (url) => {
+export const useFetch = (url, method = 'GET') => {
   const [data, setData] = useState(null);
   const [isPending, setIsPending] = useState(false);
   const [error, setError] = useState(null);
