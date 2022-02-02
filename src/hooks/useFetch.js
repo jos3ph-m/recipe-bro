3f5b1f719cc270c0535353b887115ad677d4293d
Joseph M <jmedina1985@hotmail.com>
2022-02-02 14:46:57 -0800

    Making a POST request

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -4,6 +4,7 @@ export const useFetch = (url, method = 'GET') => {
   const [data, setData] = useState(null);
   const [isPending, setIsPending] = useState(false);
   const [error, setError] = useState(null);
+  const [options, setOptions] = useState(null);
 
   const postData = (postData) => {};
 
