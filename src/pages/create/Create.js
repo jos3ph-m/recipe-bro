3f77200bc46a7f7e5caf22eddeb4dc3ed8540efa
Joseph M <jmedina1985@hotmail.com>
2022-02-03 14:40:00 -0800

    Making a POST request

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -12,7 +12,10 @@ export default function Create() {
   const [ingredients, setIngredients] = useState([]);
   const ingredientInput = useRef(null);
 
-  const { postData, data, error } = useFetch('http://localhost:3000/recipes');
+  const { postData, data, error } = useFetch(
+    'http://localhost:3000/recipes',
+    'POST'
+  );
 
   const handleSubmit = (e) => {
     e.preventDefault();
