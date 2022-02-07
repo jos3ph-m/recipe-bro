ebf895d5b03671b1389278b80d8c16d9a53e8d12
Joseph M <jmedina1985@hotmail.com>
2022-02-07 13:33:05 -0800

    Making a search bar component

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useEffect } from 'react/cjs/react.development';
 
 // import
 import './Searchbar.css';
 
 export default function Searchbar() {
   const [term, setTerm] = useState('');
+  const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    history.push('');
   };
 
   return (
