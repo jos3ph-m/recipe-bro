0941cf4865bea57ada5db9c20cf15f91062086d9
Joseph M <jmedina1985@hotmail.com>
2022-02-06 17:24:43 -0800

    Redirecting the user

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -42,8 +42,9 @@ export default function Create() {
   };
 
   // redirect the user when we get data response
-  useEffect(() => {}, [data]);
-  // if there is a change in data direct user back to home page
+  useEffect(() => {
+    // if there is a change in data direct user back to home page
+  }, [data]);
 
   return (
     <div className="create">
