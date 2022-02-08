aca1ab5406ea9668279a679fe922a5e9a78e9b0f
Joseph M <jmedina1985@hotmail.com>
2022-02-08 11:11:44 -0800

    Search results page

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,6 +1,9 @@
+import { useLocation } from 'react-router-dom';
+
 //styles
 import './Search.css';
 
 export default function Search() {
+  const queryString = useLocation();
   return <div>Search</div>;
 }
