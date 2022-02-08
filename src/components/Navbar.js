2065dacd172c8b4b9ca4f18ad3bef8f21e11e4f1
Joseph M <jmedina1985@hotmail.com>
2022-02-08 11:05:48 -0800

    Making a search bar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom';
 // styles
 import './Navbar.css';
 
+// components
+import Searchbar from './Searchbar';
+
 export default function Navbar() {
   return (
     <div className="navbar">
@@ -10,6 +13,7 @@ export default function Navbar() {
         <Link to="/" className="brand">
           <h1>Recipe Bro</h1>
         </Link>
+        <Searchbar />
         <Link to="/create">Create Recipe</Link>
       </nav>
     </div>
