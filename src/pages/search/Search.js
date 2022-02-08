3639dbbb5909863bd6d6aa838140afc8b5fd5692
Joseph M <jmedina1985@hotmail.com>
2022-02-08 11:53:12 -0800

    Search results page

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,4 +1,5 @@
 import { useLocation } from 'react-router-dom';
+import { useFetch } from '../../hooks';
 
 //styles
 import './Search.css';
@@ -7,6 +8,8 @@ export default function Search() {
   const queryString = useLocation();
   const queryParams = new URLSearchParams(queryString);
   const query = queryParams.get('q');
+
+  const url = `http://localhost:3000/recipes?q=${query}`;
 
   return <div>Search</div>;
 }
