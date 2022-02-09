349f677e34c26fc0597c851221c2561f30162f20
Joseph M <jmedina1985@hotmail.com>
2022-02-09 11:46:15 -0800

    Search results page

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -20,7 +20,6 @@ export default function Search() {
       <h2 className="page-title">Recipes including "{query}"</h2>
       {error && <p className="error">{error}</p>}
       {isPending && <p className="loading">Loading...</p>}
-      {data && <RecipeList recipes={data} />}
     </div>
   );
 }
