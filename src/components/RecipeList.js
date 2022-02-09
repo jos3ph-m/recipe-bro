db1e569b231a6a085023a0006d33b1828ff7a986
Joseph M <jmedina1985@hotmail.com>
2022-02-09 11:57:37 -0800

    Finishing touches

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -4,6 +4,10 @@ import { Link } from 'react-router-dom';
 import './RecipeList.css';
 
 export default function RecipeList({ recipes }) {
+  // check if recipe list is empty
+  if (recipes.length === 0) {
+  }
+
   return (
     <div className="recipe-list">
       {recipes.map((recipe) => (
