022b0159d7238ace3994429d7a9f8b57f9cdb16d
Joseph M <jmedina1985@hotmail.com>
2022-02-13 19:11:09 -0800

    Creating context and provider

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ThemeProvider } from './context/ThemeContext';
 
 ReactDOM.render(
   <React.StrictMode>
