76d6bb431d4714f35ea6ebfb7385dc1e9788695e
Joseph M <jmedina1985@hotmail.com>
2022-02-14 12:00:45 -0800

    Accessing Context values

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom';
-import { useContext } from 'react';
-import { ThemeContext } from '../context/ThemeContext';
+
 // styles
 import './Navbar.css';
 
---
+++ src/hooks/useTheme.js
@@ -0,0 +1,2 @@
+import { useContext } from 'react';
+import { ThemeContext } from '../context/ThemeContext';
