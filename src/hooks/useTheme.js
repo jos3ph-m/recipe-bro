40cfc1f2b426e6200b276e8cdd983b28b6db8c46
Joseph M <jmedina1985@hotmail.com>
2022-02-14 12:09:44 -0800

    Creating a Custom Context Hook

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -3,4 +3,9 @@ import { ThemeContext } from '../context/ThemeContext';
 
 export const useTheme = () => {
   const context = useContext(ThemeContext);
+
+  if (context === undefined) {
+  }
+
+  return context;
 };
