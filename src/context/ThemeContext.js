496b45d91f4d6640d520447f43732e4de2d7aa2e
Joseph M <jmedina1985@hotmail.com>
2022-02-13 19:14:20 -0800

    Finishing making provider

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -3,6 +3,8 @@ import { createContext } from 'react';
 export const ThemeContext = createContext();
 
 export function ThemeProvider({ children }) {
+  // future custom logic will go here
+
   return (
     <ThemeContext.Provider value={{ color: 'blue' }}>
       {children}
