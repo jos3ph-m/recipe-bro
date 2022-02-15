e7863ab5589ca3ebafd15478cc9d99d269140752
Joseph M <jmedina1985@hotmail.com>
2022-02-15 14:27:17 -0800

    Reducers and useReducer

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -8,6 +8,9 @@ export function ThemeProvider({ children }) {
   const [state, dispatch] = useReducer(themeReducer, {
     color: 'darkslategray',
   });
+  const changeColor = (color) => {
+    dispatch({});
+  };
   return (
     <ThemeContext.Provider value={{ color: 'darkslategray' }}>
       {children}
