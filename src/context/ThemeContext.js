7f30f117f2f40cfea279010701ad87b22e08bbb7
Joseph M <jmedina1985@hotmail.com>
2022-02-15 14:04:29 -0800

    Reducers and useReducer

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -3,8 +3,6 @@ import { createContext, useReducer } from 'react';
 export const ThemeContext = createContext();
 
 export function ThemeProvider({ children }) {
-  // future custom logic will go here
-
   return (
     <ThemeContext.Provider value={{ color: 'darkslategray' }}>
       {children}
