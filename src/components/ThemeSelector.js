f98291cc207ec050b9b10f519da4f6d53f5e2ca0
Joseph M <jmedina1985@hotmail.com>
2022-02-17 12:34:47 -0800

    Making a color theme selector

--- src/components/ThemeSelector.js
+++ src/components/ThemeSelector.js
@@ -12,7 +12,11 @@ export default function ThemeSelector() {
     <div className="theme-selector">
       <div className="theme-buttons">
         {themeColors.map((color) => {
-          <div key={color} onClick={() => changeColor(color)} />;
+          <div
+            key={color}
+            onClick={() => changeColor(color)}
+            style={{ background: color }}
+          />;
         })}
       </div>
     </div>
