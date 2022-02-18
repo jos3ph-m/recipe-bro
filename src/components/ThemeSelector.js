791529dc935c15e9a01681f6d0c19fb27deb7482
Joseph M <jmedina1985@hotmail.com>
2022-02-18 14:36:06 -0800

    Making a color theme selector

--- src/components/ThemeSelector.js
+++ src/components/ThemeSelector.js
@@ -4,7 +4,14 @@ import { useTheme } from '../hooks/useTheme';
 import './ThemeSelector.css';
 
 // swatches
-const themeColors = ['#58249c', '#249c6b', '#9f2020', '#175908', '#9f9620', ,];
+const themeColors = [
+  '#58249c',
+  '#249c6b',
+  '#9f2020',
+  '#175908',
+  '#9f9620',
+  '#a7bcb9',
+];
 
 export default function ThemeSelector() {
   const { changeColor } = useTheme();
