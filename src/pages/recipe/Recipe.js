016346d76423b94730d94594fe380d30ed3def62
Joseph M <jmedina1985@hotmail.com>
2022-02-22 11:21:22 -0800

    Light and dark mode selector

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom';
 import { useFetch } from '../../hooks/useFetch';
+import { useTheme } from '../../hooks/useTheme';
 
 //styles
 import './Recipe.css';
@@ -7,8 +8,8 @@ import './Recipe.css';
 export default function Recipe() {
   const { id } = useParams();
   const url = `http://localhost:3000/recipes/${id}`;
-
   const { error, isPending, data: recipe } = useFetch(url);
+  const { mode } = useTheme();
 
   return (
     <div className="recipe">
