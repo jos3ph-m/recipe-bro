0f9d42fbae80340231d63abe215742cbfef5001d
Joseph M <jmedina1985@hotmail.com>
2022-02-23 13:16:07 -0800

    Finishing touches

--- src/components/ThemeSelector.js
+++ src/components/ThemeSelector.js
@@ -5,7 +5,7 @@ import modeIcon from '../assets/mode-icon.svg';
 import './ThemeSelector.css';
 
 // swatches
-const themeColors = ['#9f9620'];
+const themeColors = ['#58249c', '#9f9620'];
 
 export default function ThemeSelector() {
   const { changeColor, changeMode, mode } = useTheme();
