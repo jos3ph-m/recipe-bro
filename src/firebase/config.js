ea8cd98ce3dcf1a4a048254c51e6b808907b2226
Joseph M <jmedina1985@hotmail.com>
2022-02-26 17:51:49 -0800

    Adding firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,6 +2,8 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: 'recipe-bro-site.firebaseapp.com',
   projectId: 'recipe-bro-site',
+  appId: process.env.FIREBASE_APP_ID,
 };
