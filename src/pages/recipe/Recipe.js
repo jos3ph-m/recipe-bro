7150d95387416f6c8e5a3b59b6b53c86856efae5
Joseph M <jmedina1985@hotmail.com>
2022-03-01 13:57:37 -0800

    Fetching a firestore collection

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -7,7 +7,7 @@ import './Recipe.css';
 
 export default function Recipe() {
   const { id } = useParams();
-  const url = `http://localhost:3000/recipes/${id}`;
+
   const { error, isPending, data: recipe } = useFetch(url);
   const { mode } = useTheme();
 
