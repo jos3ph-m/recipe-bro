e20386c0ec6d2a7a5834c75af3b3c0583aafa742
Joseph M <jmedina1985@hotmail.com>
2022-03-02 12:26:34 -0800

    Fetching a firestore document

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -26,6 +26,7 @@ export default function Recipe() {
           setRecipe(doc.data());
         } else {
           setIsPending(false);
+          setError('Recipe does not exist');
         }
       });
   }, []);
