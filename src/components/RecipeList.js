408dba2600fa15bec0c72b747a1ebbbe9eceed4b
Joseph M <jmedina1985@hotmail.com>
2022-03-03 15:38:16 -0800

    Adding firestore data

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -20,7 +20,7 @@ export default function RecipeList({ recipes }) {
           <p>{recipe.cookingTime} to make.</p>
           <div>{recipe.method.substring(0, 100)}...</div>
           <Link to={`/recipes/${recipe.id}`}>Cook This</Link>
-          <img />
+          <img className="delete" src={DeleteIcon} />
         </div>
       ))}
     </div>
