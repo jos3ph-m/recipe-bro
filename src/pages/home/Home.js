f4526c0bfabe7d3dace2dee48703693e570f8148
Joseph M <jmedina1985@hotmail.com>
2022-03-04 16:35:53 -0800

    Real-time collection data

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -29,7 +29,9 @@ export default function Home() {
           setIsPending(false);
         }
       },
-      (err) => {}
+      (err) => {
+        setError(err.message);
+      }
     );
   }, []);
 
