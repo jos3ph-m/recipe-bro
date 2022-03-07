335e40daf9d1956651068f10e8a4a9efaabf8eeb
Joseph M <jmedina1985@hotmail.com>
2022-03-06 16:25:18 -0800

    Updating documents - realtime document data

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -52,7 +52,7 @@ export default function Recipe() {
             ))}
           </ul>
           <p className="method">{recipe.method}</p>
-          <button onClick={handleClick}>Update me</button>
+          <button onClick={handleClick}>Now the title is something else</button>
         </>
       )}
     </div>
